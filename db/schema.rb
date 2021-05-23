c2a8de5b8dda082937863bed170bff29489ba8b7
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2021-05-23 21:06:30 +0900

    ActiveStrorage再インストール後にmigrate実施

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_063546) do
-=======
 ActiveRecord::Schema.define(version: 2021_05_13_184124) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -34,7 +32,6 @@ ActiveRecord::Schema.define(version: 2021_05_13_184124) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
->>>>>>> Stashed changes
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text", null: false
